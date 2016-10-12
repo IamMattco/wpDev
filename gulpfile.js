e6d37b61e7f41a0728136e6955ecb4a62b52c64b
dev.mroz@gmail.com <dev.mroz@gmail.com>
2016-10-12 16:26:32 +0200

    #2 create notifications default msg

--- gulpfile.js
+++ gulpfile.js
@@ -15,38 +15,41 @@ var gulp = require('gulp'),
     haml = require('gulp-ruby-haml');
 
 // Config
-
-var themeName = 'defaultTheme',
-    themePath = 'app/content/themes/'+ themeName;
+var config = {
+  themePath: 'app/content/themes/defaultTheme',
+  msg: ['Style zostały zminifikowane i połączone.',
+        'Pliki JavaScript zostały zminifikowane i połączone.',
+        'Obrazy zostały zoptymalizowane.']
+};
 
 // Styles
 gulp.task('styles', function() {
-  return sass(themePath + '/assets/src/styles/main.scss', { style: 'expanded' })
+  return sass(config.themePath + '/assets/src/styles/main.scss', { style: 'expanded' })
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest(themePath +'/assets/dist/styles'))
+    .pipe(gulp.dest(config.themePath +'/assets/dist/styles'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
-    .pipe(gulp.dest(themePath + '/assets/dist/styles'))
-    .pipe(notify({ message: 'Style zostały zminifikowane i połączone.' }));
+    .pipe(gulp.dest(config.themePath + '/assets/dist/styles'))
+    .pipe(notify({ message: config.msg[0] }));
 });
 
 // Scripts
 gulp.task('scripts', function() {
-  return gulp.src(themePath + '/assets/src/js/main.js')
+  return gulp.src(config.themePath + '/assets/src/js/main.js')
     .pipe(include())
-    .pipe(gulp.dest(themePath +'/assets/dist/js'))
+    .pipe(gulp.dest(config.themePath +'/assets/dist/js'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
-    .pipe(gulp.dest(themePath + '/assets/dist/js'))
-    .pipe(notify({ message: 'Pliki JavaScript zostały zminifikowane i połączone.' }));
+    .pipe(gulp.dest(config.themePath + '/assets/dist/js'))
+    .pipe(notify({ message: config.msg[1] }));
 });
 
 // Images
 gulp.task('images', function() {
-  return gulp.src(themePath + '/assets/src/images/*')
+  return gulp.src(config.themePath + '/assets/src/images/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest(themePath + '/assets/dist/images'))
-    .pipe(notify({ message: 'Obrazy zostały zoptymalizowane.' }));
+    .pipe(gulp.dest(config.themePath + '/assets/dist/images'))
+    .pipe(notify({ message: config.msg[3] }));
 });
 
 // Build
@@ -61,7 +64,7 @@ gulp.task('default', function() {
 
 // Watch
 gulp.task('watch', function() {
-  gulp.watch(themePath + '/assets/src/styles/*.scss', ['styles']);
-  gulp.watch(themePath + '/assets/src/js/*.js', ['scripts']);
-  gulp.watch(themePath + '/assets/src/images/*', ['images']);
+  gulp.watch(config.themePath + '/assets/src/styles/*.scss', ['styles']);
+  gulp.watch(config.themePath + '/assets/src/js/*.js', ['scripts']);
+  gulp.watch(config.themePath + '/assets/src/images/*', ['images']);
 });
